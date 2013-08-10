dc0b42ff5ed1377e5f71143067b88b8bacbd625f
Jeremiah Orem <oremj@oremj.com>
2013-08-09 18:29:15 -0700

    Add variable ping time and max requests.

--- index.js
+++ index.js
@@ -11,10 +11,14 @@ program
     .option('-s, --pushgoserver <server>', 'Push go server url, e.g., pushserver.test.com', 'push.services.mozilla.com')
     .option('-c, --clients <clients>', 'Number of client connections', Number, 1)
     .option('-C, --channels <channels>', 'Number of channels per client', Number, 1)
+    .option('-m, --minpingtime <minpingtime>', 'Minimum milliseconds between pings', Number, 500)
+    .option('-M, --maxpingtime <minpingtime>', 'Maximum milliseconds between pings', Number, 1000)
+    .option('-p, --pingsperchannel <pingsperchannel>', 'How many pings to send per channel 0 means infinite', Number, 0)
     .parse(process.argv);
 
 
-var server = new Server();
+var server = new Server(program.minpingtime, program.maxpingtime,
+                        program.pingsperchannel);
 
 function reg(m) {
     server.registerEndpoint(m);
