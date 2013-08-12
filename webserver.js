971b64a55b6729c3f3f43083ee9d4d623f7a0235
Benson Wong <mostlygeek+git@gmail.com>
2013-08-12 13:05:18 -0700

    add more web framework bits

--- webserver.js
+++ webserver.js
@@ -10,31 +10,31 @@ app.configure(function() {
     var base = fs.realpathSync(__dirname + "/web")
 
     // automatically generate css from stylus files
-    app.use(stylus.middleware({
-        src       : base + "/src/css"
-        , dest    : base + "/public/css"
+    var stylm = stylus.middleware({
+        src       : base + "/src"
+        , dest    : base + "/public/generated"
         , force   : true
+        , serve   : true
         , compile : function(str, path) {
-            stylus(str)
+            return stylus(str)
                 .set("filename", path)
                 .set("compress", false)
                 .use(nib())
-                .import('nib');
+                .import('nib')
         }
-    }));
-
+    });
+    app.use(stylm);
 
     // service static files from multiple places
-    app.use(express.static(base + "/public/static/"))
-    app.use(express.static(base + "/public/generated/"))
-
+    app.use(express.static(base + "/public/generated"))
+    app.use(express.static(base + "/public/static"))
 
     app.set("view engine", "jade");
     app.set("views", base + "/views");
 });
 
 app.get('/', function(req, res) {
-    res.send('Hello');
+    res.render("index")
 });
 
 app.use(function(err, req, res, next) {
@@ -42,7 +42,19 @@ app.use(function(err, req, res, next) {
     next();
 });
 
-var port = process.env.PORT || 3000
-app.listen(port, function() {
-    console.log("Webserver started. Listening on " + port);
-});
+function startup(cb) {
+    var port = process.env.PORT || 3000
+    app.listen(port, function(err) {
+        cb(err, port);
+    });
+}
+
+if (require.main === module) {
+    startup(function(err, port) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log("App listening on " + port);
+    });
+}
