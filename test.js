4db34f86176cb93eeee32a74dbde596f4da62c0b
Benson Wong <mostlygeek+git@gmail.com>
2013-08-14 13:29:12 -0700

    more tweaks to Endpoint test

--- test.js
+++ test.js
@@ -22,32 +22,39 @@ for(var j = 0; j < program.channels; j++) {
     c.registerChannel(uuid.v1());
 }
 
+var testy = debug('testy')
 c.on('pushendpoint', function(endpointUrl, channelID) {
     var e = new EndPoint(c, endpointUrl, channelID);
     var serverAckTime = 0;
     e.on('result', function(result) {
-        console.log(result.status, result.endpoint.channelID, result.time)
+        testy("Status: %s | time: %dms | channel: %s", 
+            result.status, result.time, result.endpoint.channelID
+        )
         switch (result.status) {
             case 'SERVER_OK':
                 serverAckTime = result.time;
                 break;
-
             case 'GOT_VERSION_OK':
-                console.log("WS Time", result.time - serverAckTime);
+                testy("WS RTT %dms", result.time - serverAckTime);
+                setTimeout(e.sendNextVersion.bind(e), 3000);
                 break;
 
             case 'ERR_VER_MISMATCH':
                 break;
 
             case 'TIMEOUT':
+                testy('TIMEOUT');
                 break;
 
             case 'ERR_SERVER': // the server returned a non 200
+                testy("Error. Server code: %s", result.data);
                 break;
 
             case 'ERR_NETWORK': // network issues?
                 break;
         }
+
+
 
     });
     e.sendNextVersion()
